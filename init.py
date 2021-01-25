aba7a3f87feae0af15806f0caaa38358b5f55a16
minty <itda@ua.pt>
2021-01-25 06:59:06 +0000

    fix

--- init.py
+++ init.py
@@ -41,8 +41,8 @@ async def on_message(ctx):
 	guild = ctx.guild.name
 	if ctx.author == client.user:
 		return
-	#if True:
-	if  ctx.author.guild_permissions.administrator:
+	if True:
+	#if  ctx.author.guild_permissions.administrator:
 		if HELP == ctx.content:
 			if guild not in database.keys():
 				await ctx.channel.send(f'{INIT_ERROR} {guild}')
